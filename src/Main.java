5f9cd6e10686a161d0e254c303a55e0e78515775
Mateusz <m.olejnik.1998@o2.pl>
2020-04-23 20:19:20 +0200

    Class Main

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.xml.stream.events.StartDocument;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main
+{
+        public static void main(String[] Args)
+        {
+                ChangeUnit p1 = new ChangeUnit();
+                p1.start();
+
+        }
+
+}
